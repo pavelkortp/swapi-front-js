83a7408534993ff06180f627cad61a2935fe0eaa
Pavlo Zhurbytskyi <pavelkortp@gmail.com>
2024-05-08 18:32:20 +0300

    Removed test files, added initial implementation of working with api swapi

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios'
+import PeopleTable from './components/PeopleTable';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const BASE_URL = 'http://localhost:3000/api/v1/people';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            people: [],
+            page: 1,
+            next: '',
+            previous: ''
+        }
+
+        this.nextPage = this.nextPage.bind(this);
+        this.setPeople = this.setPeople.bind(this);
+
+        this.setPeople(this.state.page)
+    }
+
+    render() {
+        return (
+            <div>
+                <PeopleTable people={this.state.people} />
+                <button type='button' id='next' onClick={this.nextPage}>NEXT</button>
+            </div>
+        )
+    }
+
+    setPeople(page) {
+        axios.get(`${BASE_URL}/?page=${page}`).then((res) => {
+            console.log(res.data);
+            this.setState({
+                people: res.data,
+                next: res.data.next,
+                previous: res.data.previous,
+                page
+            });
+        });
+    }
+
+    nextPage() {
+        if (this.state.next) {
+            this.setPeople(this.state.page + 1)
+        }else{
+            alert('Це поки остання сторінка')
+        }
+    }
 }
 
 export default App;
---
+++ src/components/PeopleTable.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import People from './People';
+
+
+class PeopleTable extends React.Component {
+    render() {
+        return (
+            <div className='container'>
+                <h1>People table</h1>
+                <table className='table table-dark table-hover'>
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Birth Year</th>
+                            <th scope="col">Gender</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        {this.props.people.map((p) => (
+                            <People key={p.url.match(/(\d+)$/)[1]} data={p} />
+
+                        ))}
+                    </tbody>
+
+
+                </table>
+            </div>
+
+        )
+    }
+}
+
+export default PeopleTable;
