cad627f04cfc8e9bc3cee2d96cb64a632978cdd8
Pavlo Zhurbytskyi <pavelkortp@gmail.com>
2024-05-22 15:38:04 +0300

    add scrols vert and horizont

--- src/components/EntitiesTable.jsx
+++ src/components/EntitiesTable.jsx
@@ -1,27 +1,32 @@
 import React from 'react';
 import EntityRow from './EntityRow';
+import '../styles/entityTable.css'
 
 export const ITEMS_PER_PAGE = 10;
 
 export default class EntitiesTable extends React.Component {
     render() {
         return (
-            <div className='container'>
-                <h1>{}</h1>
-                <table className='table table-light table-hover table-striped-columns'>
-                    <thead className='table-dark'>
-                        <tr>
-                            <th scope="col">#</th>
-                            {this.getScheme(this.props.items[0])}
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.props.items.map((item) => (
-                            <EntityRow key={item.url} data={item} />
-                        ))}
-                    </tbody>
-                </table>
-            </div>
+            <>
+                <h2>{this.props.items[0].url.split('/')[4]} table</h2>
+                <div className='table-wrapper'>
+                    <div className='container'>
+                        <table className='table table-light table-hover table-striped-columns'>
+                            <thead className='table-dark'>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    {this.getScheme(this.props.items[0])}
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {this.props.items.map((item) => (
+                                    <EntityRow key={item.url} data={item} />
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </>
         );
     }
 
