4dfa3ce17265ebcd38044fb78edeb5c88e17afee
Pavlo Zhurbytskyi <pavelkortp@gmail.com>
2024-06-10 02:18:39 +0300

    moved to another directory

---
+++ src/interfaces/Entities.ts
@@ -0,0 +1,8 @@
+import {Planet} from '../models/Planet';
+import {Film} from '../models/Film';
+import {People} from '../models/People';
+import {Specie} from '../models/Specie';
+import {Starship} from '../models/Starship';
+import {Vehicle} from '../models/Vehicle';
+
+export type Entities = Planet | Film | People | Specie | Starship | Vehicle;
--- src/models/Entities.ts
+++
@@ -1,8 +0,0 @@
-import {Planet} from './Planet';
-import {Film} from './Film';
-import {People} from './People';
-import {Specie} from './Specie';
-import {Starship} from './Starship';
-import {Vehicle} from './Vehicle';
-
-export type Entities = Planet | Film | People | Specie | Starship | Vehicle;
