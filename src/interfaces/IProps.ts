b10eb17e4221eff3568df6264d39fa7474978264
Pavlo <pavelkortp@gmail.com>
2024-06-21 15:34:37 +0300

    add new field

--- src/interfaces/IProps.ts
+++ src/interfaces/IProps.ts
@@ -1,4 +1,5 @@
-import {GroupBase, OptionsOrGroups} from 'react-select';
+import {GroupBase, MultiValue, OptionsOrGroups, SingleValue} from 'react-select';
+import {Entities} from './Entities';
 
 export interface HTMLInputProps {
     value?: string;
@@ -21,10 +22,16 @@ export interface TagInputProps extends InputProps {
     handleOnInputChange: (text: string) => void;
     options: OptionsOrGroups<Tag, GroupBase<Tag>>;
     isMulti?: boolean;
+    value?: SingleValue<Tag> | MultiValue<Tag>
 }
 
 export interface CreationFormProps {
     onCreate: (data: FormData) => void;
+}
+
+export interface UpdateFormProps {
+    onUpdate: (data: FormData) => void;
+    existedData: Entities;
 }
 
 export interface FormControlsProps {
